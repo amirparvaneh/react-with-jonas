c922dbf0846418f0007ac6b0a52935555217be7c
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-11 00:08:28 +0330

    challenge-1

--- challenge-01/challenge-1/src/index.js
+++ challenge-01/challenge-1/src/index.js
@@ -8,16 +8,53 @@ function App() {
       <Avatar />
       <div className="data">
         <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
         <SkillList />
       </div>
     </div>
   );
 }
 
+const Avatar = () => {
+  return (
+    <div>
+      <img className="avatar" src="avatar-pic.jpg" alt="amir parvaneh" />
+    </div>
+  );
+};
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Amir parvaneh</h1>
+      <p>
+        full-stack software developer passion to solve hard challenges... and
+        love to work in big companies with really challenging projects...
+      </p>
+    </div>
+  );
+};
+
+const SkillList = () => {
+  return (
+    <div className="skill-list">
+      <Skill skill="react" emoji="💪" color="blue" />
+      <Skill skill="java" emoji="💪" color="white" />
+      <Skill skill="javascript" emoji="💪" color="red" />
+    </div>
+  );
+};
+
+const Skill = (props) => {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+};
+
 const rootElement = document.getElementById("root");
+
 const root = createRoot(rootElement);
 
 root.render(
