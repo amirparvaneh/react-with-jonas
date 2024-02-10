ea2e52bf0f1ba8d422e464504bae8b2e00391d25
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-10 13:58:52 +0330

    pizza-menu

---
+++ 03-pizza-menu/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  <h1> Hello react</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
