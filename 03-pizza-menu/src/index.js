3e2b3cdde7cb90c7bb01a2dd63457f2bd527bfd5
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-10 21:16:55 +0330

    pizza-menu

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  <h1> Hello react</h1>;
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
