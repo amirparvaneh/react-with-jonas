b587258ba585789258d9c322daccbfc24bedce14
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-14 23:27:00 +0330

    steps-04

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -1,3 +1,26 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
-  return <div>hello React</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">{messages}</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          pervious
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
