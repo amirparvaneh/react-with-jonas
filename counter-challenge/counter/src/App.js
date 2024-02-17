11c2e7c9c2c2101fc76b04ad472b06331e3cc02c
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-17 21:05:13 +0330

    counter-challenge

--- counter-challenge/counter/src/App.js
+++ counter-challenge/counter/src/App.js
@@ -1,8 +1,43 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
-export default App;
+const Counter = () => {
+  const [count, setCounter] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("Feb 18 2024");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step : {step} </span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCounter((c) => c - step)}>-</button>
+        <span>Count : {count} </span>
+        <button onClick={() => setCounter((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{`${date}`}</span>
+      </p>
+    </div>
+  );
+};
