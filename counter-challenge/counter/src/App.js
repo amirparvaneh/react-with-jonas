4187fe236159e74bd2393a42e406f815aa7e1b6c
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-20 19:26:23 +0330

    counter with range

--- counter-challenge/counter/src/App.js
+++ counter-challenge/counter/src/App.js
@@ -13,19 +13,34 @@ const Counter = () => {
   const [count, setCounter] = useState(0);
   const [step, setStep] = useState(1);
 
+  const handleReset = () => {
+    setCounter(0);
+    setStep(1);
+  };
   const date = new Date("Feb 18 2024");
   date.setDate(date.getDate() + count);
 
   return (
     <div>
       <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Step : {step} </span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+        <span>Step : {step}</span>
       </div>
       <div>
         <button onClick={() => setCounter((c) => c - step)}>-</button>
-        <span>Count : {count} </span>
+        <span>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCounter(Number(e.target.value))}
+          />
+        </span>
         <button onClick={() => setCounter((c) => c + step)}>+</button>
       </div>
       <p>
@@ -38,6 +53,9 @@ const Counter = () => {
         </span>
         <span>{`${date}`}</span>
       </p>
+      <div>
+        <button onClick={handleReset}>reset</button>
+      </div>
     </div>
   );
 };
