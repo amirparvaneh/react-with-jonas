ae1d3e199e007337b55f9abcaf76f7dce169aae3
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-23 15:47:47 +0330

    excercise accordionj

---
+++ excercise-Accordion/accordion-item/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
+}
+
+const Accordion = () => {};
+
+export default App;
