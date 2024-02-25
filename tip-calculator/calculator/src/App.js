38cb175b023d39f20e0e2f40b4e963992ae77bde
amirparvaneh <amirparvaneh138@gmail.com>
2024-02-25 23:58:15 +0330

    tip calculator challenge

--- tip-calculator/calculator/src/App.js
+++ tip-calculator/calculator/src/App.js
@@ -4,19 +4,76 @@ import "./styles.css";
 function App() {
   return (
     <div>
-      <div></div>
+      <TipCalculator />
     </div>
   );
 }
 
-const TipCalculator = () => {};
+const TipCalculator = () => {
+  const [bill, setBill] = useState("");
+  const [percentage1, stePercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
 
-const BillInput = () => {};
+  const tip = (bill * ((percentage1 + percentage2) / 2)) / 100;
 
-const SelectPercentage = () => {};
+  const handleReset = () => {
+    setBill("");
+    stePercentage1(0);
+    setPercentage2(0);
+  };
 
-const OutPut = () => {};
+  return (
+    <div>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={percentage1} onSelect={stePercentage1}>
+        How much did you like the service ?
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How much your friend like the service?
+      </SelectPercentage>
+      <OutPut tip={tip} onHandle={handleReset} />
+      <Reset />
+    </div>
+  );
+};
 
-const Reset = () => {};
+const BillInput = () => {
+  return (
+    <div>
+      <label>How much was the bill? </label>
+      <input type="text" placeholder="Bill value" />
+    </div>
+  );
+};
+
+const SelectPercentage = ({ percentage, onSelect, children }) => {
+  return (
+    <div value={percentage} onChange={(e) => onSelect(Number(e.target.value))}>
+      <label>{children}</label>
+      <select>
+        <option value="0">Disatisfied 0%</option>
+        <option value="5">It was okay 5%</option>
+        <option value="10">It was good 10%</option>
+        <option value="20">Absoloutly okay 20%</option>
+      </select>
+    </div>
+  );
+};
+
+const OutPut = ({ bill, tip }) => {
+  return (
+    <h3>
+      you pay ${bill + tip} (${bill} + ${tip})
+    </h3>
+  );
+};
+
+const Reset = ({ onHandle }) => {
+  return (
+    <div onClick={onHandle}>
+      <button>Reset</button>
+    </div>
+  );
+};
 
 export default App;
