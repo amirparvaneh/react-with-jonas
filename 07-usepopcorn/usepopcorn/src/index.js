4b7d2d5da34c9e089ee4f424159bd7e380599ee8
amirparvaneh <amirparvaneh138@gmail.com>
2024-03-05 14:12:28 +0330

    usepopcorn add custom component

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StartRating";
 // import "./index.css";
 // import App from "./App";
 
+const Test = () => {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p>the movie was rated {movieRating}</p>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating maxRating={5} className="test" color="red" defaultRating={4} />
     {/* <App /> */}
+    <Test />
   </React.StrictMode>
 );
