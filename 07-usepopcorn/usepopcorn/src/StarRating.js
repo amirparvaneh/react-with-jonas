306059a64955d3c27d854ae155a16f0a84a34ad8
amirparvaneh <amirparvaneh138@gmail.com>
2024-03-11 22:45:20 +0330

    complete star rating

--- 07-usepopcorn/usepopcorn/src/StarRating.js
+++ 07-usepopcorn/usepopcorn/src/StarRating.js
@@ -9,7 +9,6 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 
 const StarRating = ({
@@ -85,7 +84,6 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
           viewBox="0 0 20 20"
           fill={color}
           stroke={color}
-          fontSize={size}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -108,7 +106,7 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
   );
 };
 
-StarRating.prototype = {
+StarRating.prototypes = {
   maxRating: PropTypes.number,
   defaultRating: PropTypes.number,
   color: PropTypes.string,
