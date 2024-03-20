ce3f276626c9b03a15b8c8d36b430b5f6e9f8788
amirparvaneh <amirparvaneh138@gmail.com>
2024-03-20 16:21:38 +0330

    start project

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,9 +1,15 @@
 import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 const App = () => {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 };
