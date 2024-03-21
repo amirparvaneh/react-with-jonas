0c512ede351357aa67f71fb79e79924694384539
amirparvaneh <amirparvaneh138@gmail.com>
2024-03-21 09:06:18 +0330

    add fake server

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,8 +1,41 @@
+import { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+};
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
