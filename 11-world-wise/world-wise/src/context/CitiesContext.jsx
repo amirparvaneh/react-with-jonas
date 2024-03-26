14c31ccde5e6dfe268fca39ee41ed4df61d5ef2d
amirparvaneh <amirparvaneh138@gmail.com>
2024-03-26 09:21:10 +0330

    add city view in city component

--- 11-world-wise/world-wise/src/context/CitiesContext.jsx
+++ 11-world-wise/world-wise/src/context/CitiesContext.jsx
@@ -9,6 +9,7 @@ const CitiesContext = createContext();
 const CitiesProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [cities, setCities] = useState([]);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     const fetchCities = async () => {
@@ -26,11 +27,26 @@ const CitiesProvider = ({ children }) => {
     fetchCities();
   }, []);
 
+  const getCity = async (id) => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("Error in loading city...");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
