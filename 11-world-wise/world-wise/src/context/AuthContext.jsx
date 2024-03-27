9656b576cb4c155284e75d91f51ddcc4c6818014
amirparvaneh <amirparvaneh138@gmail.com>
2024-03-27 17:39:57 +0330

    AuthContext and useAuth hook

---
+++ 11-world-wise/world-wise/src/context/AuthContext.jsx
@@ -0,0 +1,72 @@
+import { createContext, useContext, useReducer } from "react";
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+const initialState = {
+  user: null,
+  isAuthenticate: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticate: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        user: null,
+        isAuthenticate: false,
+      };
+    default:
+      throw new Error("Unknown action");
+  }
+};
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [{ user, isAuthenticate }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const login = (email, password) => {
+    if (FAKE_USER.email === email && FAKE_USER.password === password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  };
+
+  const logout = () => {
+    dispatch({ type: "logout" });
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticate,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("the auth context is used out of context provider");
+  return context;
+};
+
+export { AuthProvider, useAuth };
