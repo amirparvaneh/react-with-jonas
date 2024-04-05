ad123d0786b5e269fa683a28453faaa3c740dfc6
amirparvaneh <amirparvaneh138@gmail.com>
2024-04-05 16:27:14 +0330

    add appLayout

--- 15-fast-react-pizza/fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/fast-react-pizza/src/App.jsx
@@ -1,5 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const route = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order", element: <Order /> },
+      { path: "/order/new", element: <CreateOrder /> },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={route} />;
 };
 
 export default App;
---
+++ 15-fast-react-pizza/fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast react pizza</Link>
+      <p>profileName</p>
+    </header>
+  );
+};
+
+export default Header;
