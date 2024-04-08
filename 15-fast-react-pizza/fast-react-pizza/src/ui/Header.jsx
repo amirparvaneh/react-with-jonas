9897b2aaaea14d2242a9266a8f039f73881db3e0
amirparvaneh <amirparvaneh138@gmail.com>
2024-04-08 23:58:35 +0330

    tailwind text css

--- 15-fast-react-pizza/fast-react-pizza/src/ui/Header.jsx
+++ 15-fast-react-pizza/fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast react pizza</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast react pizza
+      </Link>
       <SearchOrder />
+      <UserName />
       <p>profileName</p>
     </header>
   );
