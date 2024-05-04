819ef31579658bb032b51595b76a081aa04f1dc4
amirparvaneh <amirparvaneh138@gmail.com>
2024-05-04 14:03:34 +0330

    add styled component to project

--- 17-the-wild-oasis/wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/wild-oasis/src/App.jsx
@@ -1,8 +1,42 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  cursor: pointer;
+  margin: 20px;
+  color: white;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello vite</h1>
-    </div>
+    <StyledApp>
+      <H1>Hello vite</H1>
+      <Button onClick={() => alert("Check in")}>Check in</Button>
+      <Button onClick={() => alert("Check out")}>Check out</Button>
+      <Input type="number" placeholder="Number of gusts" />
+    </StyledApp>
   );
 };
 
