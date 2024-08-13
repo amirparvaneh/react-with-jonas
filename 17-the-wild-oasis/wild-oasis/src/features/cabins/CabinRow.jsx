5fdfeca200ceb7b45a68a151413e71bec9bbe7d0
amirparvaneh <amirparvaneh138@gmail.com>
2024-08-13 12:01:20 +0330

    add feature

--- 17-the-wild-oasis/wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17-the-wild-oasis/wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation } from "@tanstack/react-query";
+import { deleteCabins } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -41,7 +43,24 @@ const Discount = styled.div`
 `;
 
 const CabinRow = ({ cabin }) => {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabins,
+    onSuccess: () => {
+      alert("cabin successfully deleted.");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => alert(err.message),
+  });
   return (
     <TableRow>
       <Img scr={image} />
@@ -49,6 +68,9 @@ const CabinRow = ({ cabin }) => {
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
   );
 };
--- 17-the-wild-oasis/wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/wild-oasis/src/services/apiCabins.js
@@ -10,3 +10,12 @@ export const getCabins = async () => {
 
   return data;
 };
+
+export const deleteCabins = async (id) => {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error.message);
+    throw new Error("Cabins could not be deleted");
+  }
+  return data;
+};
